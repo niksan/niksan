78fae4aaccd80f88ec72350f5aceffdf4b7bf253
NikSan <info@niksan.ru>
2011-09-25 21:13:44 +0400

    add some changes

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'activeadmin'
 gem "meta_search",    '>= 1.1.0.pre'
 
 gem 'gritter_notices'
+gem 'ckeditor'
 gem 'execjs'
 gem 'therubyracer'
 gem 'sass-rails', "  ~> 3.1.0"
--- app/admin/sites.rb
+++ app/admin/sites.rb
@@ -1,3 +1,32 @@
 ActiveAdmin.register Site do
+
+  menu :label => "Сайты"
+
+  filter :name
+  filter :url
+  filter :date
+
+  index do
+    column 'Название', :name
+    column 'URL', :url
+    column 'Дата', :date
+    column 'Изображение', :image do |site|
+      image_tag site.image.url(:thumb)
+    end
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "Содержание" do
+      f.input :name
+      f.input :url
+      f.input :description
+      f.input :image
+      f.input :date
+      f.input :task
+      f.input :solution
+    end
+    f.buttons
+  end
   
 end
