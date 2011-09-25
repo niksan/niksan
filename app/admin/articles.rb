842b30fd6f72b8f26e78849f0eb348675dd092c2
NikSan <info@niksan.ru>
2011-09-25 18:07:33 +0400

    install activeadmin

---
+++ app/admin/articles.rb
@@ -0,0 +1,5 @@
+ActiveAdmin.register Article do
+  menu :label => "Статьи"
+
+  filter :title
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Niksan2::Application.routes.draw do
 
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   root :to => 'main#index'
   resources :main, :articles, :sites, :contacts, :portfolio
 
