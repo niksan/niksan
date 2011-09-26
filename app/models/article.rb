fb4f0e12329508527e9f5c7c97097fb1b49230a7
«niksan» <info@niksan.ru>
2011-09-26 14:21:19 +0400

    add first steps

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,11 +2,11 @@ class Article < ActiveRecord::Base
   has_many :tags, :through => :link_tags
   has_many :link_tags, :as => :tagged
 
-  #has_many :tags, :through => :article_tags
-
   validates :title, :presence => true
 
-  #typograf :body, :use_p => false, :use_br => false
-  #typograf :title, :use_p => false, :use_br => false
+  
+  def to_param
+    "#{id}-#{title}"
+  end
 
 end
