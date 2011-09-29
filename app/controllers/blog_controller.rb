63dd949a855f6cf131f9a40712cafd8bb66008ab
«niksan» <info@niksan.ru>
2011-09-29 14:06:39 +0400

    change ordering in blog_controller

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,12 +1,12 @@
 class BlogController < ApplicationController
 
   def index
-    @posts = Post.includes(:blog, :tags).order('date')
+    @posts = Post.includes(:blog, :tags).order('date DESC')
   end
 
   def show
     @blog = Blog.find(params[:id])
-    @posts = Post.where('blog_id = ?', @blog.id).includes(:tags).order('date')
+    @posts = Post.where('blog_id = ?', @blog.id).includes(:tags).order('date DESC')
   end
 
 end
